b2926859a3655bded766ca2a72be24aac97384ec
Shinichi <miyakawashi@gmail.com>
2021-11-14 19:34:38 -0500

    final touches to password generator

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 // Assignment code here
 
 
-// ---- START NOTE ----
-// Perhaps consolidate all these functions into one and return multiple boolean values.
-// Could cause issue with readabilty but less repeating.
-// ---- END NOTE ---
-
 // Prompt user for password length (User Input)
 var passwordLength = function() {
   var passLength = parseInt(window.prompt("Please enter a password length between 8 and 128 characters"));
@@ -30,16 +25,26 @@ var passwordSpecial = function() {
   return(confirmSpecial);
 }
 
-// Prompt user if they would like to include case sensitive characters in their password, return boolean
-// If false, user to keep lowercase letters. If yes, both lowercase and uppercase characters.
-var passwordCaseSensitive = function() {
-  var confirmCap = window.confirm("Would you like your password to be case sensitive? This will include both Uppercase and Lowercase characters");
+// Prompt user if they would like to include case sensitive characters in their password.
+var passwordUpperCase = function() {
+  var confirmCap = window.confirm("Would you like your password to include uppercase characters?");
   if (confirmCap){
-    window.alert("You have chosen to secure your password using case sensitive characters");
+    window.alert("You have chosen to secure your password using uppercase characters");
   }else{
-    window.alert("We understand you would not like to include case sensitive characters");
+    window.alert("We understand you would not like to include uppercase characters");
   }
   return(confirmCap);
+}
+
+// Prompt user if they would like to include lowercase characters in their password.
+var passwordLowercase = function() {
+  var confirmLow = window.confirm("Would you like your password to include lowercase characters?");
+  if (confirmCap){
+    window.alert("You have chosen to secure your password using lower characters");
+  }else{
+    window.alert("We understand you would not like to include lowercase characters");
+  }
+  return(confirmLow);
 }
 
 // Prompt user if they would like to include numeric values in their password. Return boolean
@@ -79,12 +84,8 @@ var pickRandomChar = function(arr) {
   return (randomChar);
 }
 
-// Password scrambler. Taking inputs of all prompts, and return a randomized string of associated characters.
-// loop through each character in password.
-// Have random number generated for each prompt user selects as true.. i.e. (rand1=confirmSpecial, rand2=confirmCap, rand3=confrimNum)
-// select largest random number as the one who 'wins' the character spot. 
-// randomly generate a character or number for that space.
-var passwordScrambler = function(passLength, confirmSpecial, confirmCap, confirmNum) {
+// Password scrambler. Taking inputs of all prompts, and return a randomized string of associated characters
+var passwordScrambler = function(passLength, confirmSpecial, confirmCap, confirmLow, confirmNum) {
 
   // List of Special characters
   var specialChar = ["`", "~", "!","@","#","$", "%", "^", "&", "*", "(", ")","-","_","+","=",",","<",".",">","/","?",";",":","'","{","[","]","}", "\\","|"];
@@ -95,68 +96,59 @@ var passwordScrambler = function(passLength, confirmSpecial, confirmCap, confirm
   // List of uppercase characters
   var uppercaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R","S","T","U","V","W","X","Y","Z"];
 
-
+  // Initialize variables used for loop
   password = "";
+  // Counters for frequency of char type usage
+  var numSpecial = 1;
+  var numUpper = 1;
+  var numLower = 1;
+  var numNumber = 1;
+  // placeholder for character
+  var char = "";
 
-  var i = 0;
   while (password.length < passLength) {
-    randomNumbers = [
-      Math.random(), 
-      Math.random(), 
-      Math.random(),
-      Math.random()
-    ];
+    // array for char type weights
+    charTypeWeights = [
+      Math.random()*(1/numSpecial), 
+      Math.random()*(1/numUpper),
+      Math.random()*(1/numLower),
+      Math.random()*(1/numNumber),
+    ];  
 
-    var index = indexOfMax(randomNumbers);
+    // obtain index of max weight (winner)
+    var index = indexOfMax(charTypeWeights);
 
+    // conditionals for index winner
     if (index === 0 && confirmSpecial) {
-      passowrd = password.concat(pickRandomChar(specialChar));
+      numSpecial++;
+      char = pickRandomChar(specialChar);
     } else if (index === 1 && confirmCap) {
-      password = password.concat(pickRandomChar(uppercaseChar));
-    } else if (index === 2 && !confirmCap) {
-      password = password.concat(pickRandomChar(lowercaseChar));
-    } else{
-      password = password.concat(Math.floor(Math.random()*1000));
+      numUpper++;
+      char = pickRandomChar(uppercaseChar);
+    } else if (index === 2 && confirmLow) {
+      numLower++;
+      char = pickRandomChar(lowercaseChar);
+    } else if (index === 3 && confirmNum){
+      numNumber++;
+      char = Math.floor(Math.random()*9);
+    } else {
+
     }
+    password = password.concat(char);
   }
   return (password);
-}
-
-// Function that returns an array the size of the password length with values indicating Special, Uppercase, Lowercase, and numbers
-var testing = function(passLength, confirmSpecial, confirmCap, confirmNum) {
-
-  // Initialize an array of weights for each character in the password.
-  var weights = new Array(passLength).fill(0);
-
-  // randomize 4 numbers to select which number 'wins' the character slot for password. 
-  // 0: special, 1: Uppercase, 2: lowercase, 3: numbers
-  var criteriaWeight = [Math.random(), Math.random(), Math.random(), Math.random()];
-
-
-  for (var i=0; i < weights.length; i++) {
-    var placeHolder = indexOfMax(criteriaWeight);
-
-    if (placeHolder === 0 && confirmSpecial) {
-      weights[i] = placeHolder;
-    } else if (placeHolder === 1 && confirmCap) {
-      weights[i] = placeHolder;
-    } else if (placeHolder === 2 && !confirmCap) {
-      weights[i] = placeHolder;
-    } else if (placeHolder === 3 && confirmNum){
-      weights[i] = placeHolder;
-    }
-  }
-  return(weights);
 }
 
 var generatePassword = function() {
   passwordLength = passwordLength();
   confirmSpecial = passwordSpecial();
-  confirmCap = passwordCaseSensitive();
+  confirmCap = passwordUpperCase();
+  confirmLow = passwordLowercase();
   confirmNum = passwordNumeric();
 
-  password = passwordScrambler(passwordLength, confirmSpecial, confirmCap, confirmNum);
-  console.log(password);
+  password = passwordScrambler(passwordLength, confirmSpecial, confirmCap, confirmLow, confirmNum);
+
+  window.alert("your password is: " + password);
 }
 
 // Get references to the #generate element
